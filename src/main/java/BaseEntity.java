cd86ba3e93b34b933709195a02fd914b494a16ce
mahan <mn.nakhei98@gmail.com>
2023-06-07 22:37:10 +0430

    created BaseEntity class in project

---
+++ src/main/java/BaseEntity.java
@@ -0,0 +1,19 @@
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@MappedSuperclass
+public class BaseEntity <ID extends Serializable> implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    ID id;
+}
