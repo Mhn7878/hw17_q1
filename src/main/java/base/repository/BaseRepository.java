b30a85ebd8cee4ef6d602fe3d421356f4068a2b6
mahan <mn.nakhei98@gmail.com>
2023-06-07 23:34:08 +0430

    added save,update,remove,findAll,findById to repository class in project

--- src/main/java/base/repository/BaseRepository.java
+++ src/main/java/base/repository/BaseRepository.java
@@ -3,6 +3,17 @@ package base.repository;
 import base.BaseEntity;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
 
 public interface BaseRepository <T extends BaseEntity<ID>, ID extends Serializable>{
+    T save(T entity);
+
+    Optional<T> findById(ID id);
+
+    Boolean remove(T entity);
+
+    List<T> findAll();
+
+    T update(T entity);
 }
--- src/main/java/base/repository/impl/BaseRepositoryImpl.java
+++ src/main/java/base/repository/impl/BaseRepositoryImpl.java
@@ -3,8 +3,62 @@ package base.repository.impl;
 import base.BaseEntity;
 import base.repository.BaseRepository;
 
+import javax.persistence.EntityManager;
 import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
 
-public class BaseRepositoryImpl <T extends BaseEntity<ID>,
+public abstract class BaseRepositoryImpl<T extends BaseEntity<ID>,
         ID extends Serializable> implements BaseRepository<T, ID> {
+    protected final EntityManager em;
+
+    public abstract Class<T> getEntityClass();
+
+
+    public BaseRepositoryImpl(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public T save(T entity) {
+        if (entity.getId() == null) {
+            em.persist(entity);
+            return entity;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Optional<T> findById(ID id) {
+        return Optional.ofNullable(em.find(getEntityClass(), id));
+    }
+
+
+    @Override
+    public Boolean remove(T entity) {
+        if (entity.getId() != null) {
+            em.remove(entity);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public List<T> findAll() {
+        return em.createQuery(
+                "from " + getEntityClass().getSimpleName(), getEntityClass()
+        ).getResultList();
+    }
+
+    @Override
+    public T update(T entity) {
+        if (entity.getId() != null) {
+            em.merge(entity);
+            return entity;
+        } else {
+            return null;
+        }
+    }
 }
